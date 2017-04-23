98ffdae06ec6c497d04f16cda8e8a06b3d4c23f7
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-04-23 12:51:23 +0200

    Fix some units in test_Telemetry

--- test_telemetry.sh
+++ test_telemetry.sh
@@ -1,20 +1,20 @@
 while true; do
     mosquitto_pub -t device_beacon -m 1
     mosquitto_pub -t 1/camera_stream/0 -m http://127.0.0.1:8090/0.webm
-    mosquitto_pub -t 1/telemetry/accelX -m 0.123
-    mosquitto_pub -t 1/telemetry/accelY -m 0.123
-    mosquitto_pub -t 1/telemetry/accelZ -m 9.7
+    mosquitto_pub -t 1/telemetry/accelX -m 12.3
+    mosquitto_pub -t 1/telemetry/accelY -m 12.3
+    mosquitto_pub -t 1/telemetry/accelZ -m 970
     mosquitto_pub -t 1/telemetry/acceldX -m 0.123
     mosquitto_pub -t 1/telemetry/acceldY -m 0.123
     mosquitto_pub -t 1/telemetry/acceldZ -m 9.7
     mosquitto_pub -t 1/telemetry/speed -m 3
     mosquitto_pub -t 1/telemetry/enginePower -m 3
-    mosquitto_pub -t 1/telemetry/steeringAngle -m 3
-    mosquitto_pub -t 1/telemetry/voltage/battery -m 7.2
-    mosquitto_pub -t 1/telemetry/voltage/cell1 -m 3.1
-    mosquitto_pub -t 1/telemetry/voltage/cell2 -m 4.1
-    mosquitto_pub -t 1/telemetry/voltage/3v3bus -m 3.4
-    mosquitto_pub -t 1/telemetry/voltage/vcc -m 5.1
+    mosquitto_pub -t 1/telemetry/steeringAngle -m 90
+    mosquitto_pub -t 1/telemetry/voltage/battery -m 7200
+    mosquitto_pub -t 1/telemetry/voltage/cell1 -m 3100
+    mosquitto_pub -t 1/telemetry/voltage/cell2 -m 4100
+    mosquitto_pub -t 1/telemetry/voltage/3v3bus -m 3400
+    mosquitto_pub -t 1/telemetry/voltage/vcc -m 5100
     mosquitto_pub -t 1/telemetry/temperature/ambient -m 20.7
     mosquitto_pub -t 1/telemetry/temperature/motor -m 10.3
     mosquitto_pub -t 1/telemetry/temperature/esc -m 30.1
